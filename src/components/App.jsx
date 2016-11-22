c03e9968d0092c93f3533acdaa888362e6155695
Valeria Di Gregorio <clear>
2016-11-22 15:07:03 -0500

    simple nav component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./App.css";
+import Nav from "./Nav/Nav.js";
 
 
 export default class App extends Component {
@@ -6,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-     HELLO WORLD
+        <Nav />
       </div>
       );
   }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+export default class Nav extends Component {
+
+  render() {
+    return (
+      <div>
+        <div id="header">
+          <h1>TechScout</h1>
+          <button>Sign Up</button>
+          <button>Log In</button>
+        </div>
+      </div>
+      );
+  }
+
+}
