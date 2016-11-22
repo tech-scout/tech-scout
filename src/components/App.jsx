010be26aeab23cbac8366243c22709b9a13d8b1e
Imani Fecu <imani.fecu72@ymail.com>
2016-11-22 15:49:21 -0500

    beginning events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import EventList from './EventList/EventList.jsx';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div>
-     HELLO WORLD
+      <div className="event_list">
+     <EventList />
       </div>
       );
   }
