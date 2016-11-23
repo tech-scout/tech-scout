1dc50e68f408cbd836a9ea6f97c28d0f51aa58a9
Paris Leach <parisleach@gmail.com>
2016-11-23 10:50:11 -0500

    updated schema

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const logger  = require('morgan');
 const path    = require('path');
 const bodyParser = require('body-parser');
 
-
 const app     = express();
 const PORT    = process.argv[2] || process.env.port || 3000;
 
@@ -13,9 +12,6 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 
-
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
 
 app.listen(PORT, () => console.log('server is up and running on port', PORT));
