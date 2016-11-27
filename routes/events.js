a37ba8474a10fd94cf779151af01c8c54bea9423
Valeria Di Gregorio <clear>
2016-11-27 11:59:24 -0500

    updated

--- routes/events.js
+++ routes/events.js
@@ -14,11 +14,11 @@ const sendJSONresp = (req, res) => res.json(res.rows);
 // events
 // this is the most general route, so it goes last
 events.route('/')
-<<<<<<< HEAD
+
   .get(db.getEvents, sendJSONresp)
-=======
+
   .get(db.getAllEvents, sendJSONresp)
->>>>>>> e57b1e60aa5faf7bf5d1941ecb8bdf282a1cb05a
+
   .post(db.addEvent, sendJSONresp);
 
 // export this so it is available to server.js
--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -20,7 +20,7 @@ export default function EventForm(props) {
     <form className="form-inline" onSubmit={handleSubmit}>
 
       <div className="form-group">
-<<<<<<< HEAD
+
         <label className="sr-only" htmlFor="eventName">Event Name</label>
         <input type="text" className="form-control input-lg" name="EventTitle" placeholder="Event Name" />
       </div>
@@ -33,7 +33,7 @@ export default function EventForm(props) {
       <div className="form-group">
         <label className="sr-only" htmlFor="EventUrl">Event Url</label>
         <input type="text" className="form-control input-lg" name="url" placeholder="Event Url" />
-=======
+
         <label className="sr-only" htmlFor="eventTitle">Event Title</label>
         <input type="text" className="form-control input-lg" name="eventTitle" placeholder="Event Title" />
       </div>
@@ -46,7 +46,7 @@ export default function EventForm(props) {
       <div className="form-group">
         <label className="sr-only" htmlFor="eventUrl">Event Url</label>
         <input type="text" className="form-control input-lg" name="eventUrl" placeholder="Event Url" />
->>>>>>> e57b1e60aa5faf7bf5d1941ecb8bdf282a1cb05a
+
       </div>
 
       <button type="submit" className="btn btn-danger btn-lg" onClick={props.addEvent}>Add Event</button>
