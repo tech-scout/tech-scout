04e0811499654e51500ae38c7013e1238d607a03
Imani Fecu <imani.fecu72@ymail.com>
2016-11-27 20:46:07 -0500

    looks

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -1,69 +1,26 @@
 import React, { Component } from 'react';
-import EventList from '../components/EventList/EventList.jsx';
-import "./ProfilePage.css";
-import Nav from "../components/Nav/Nav.jsx";
-import About from '../components/About/About.jsx';
-import EventForm from '../components/EventForm/EventForm.jsx';
+import './ProfilePage.css';
+import Nav from '../components/Nav/Nav.jsx';
 import Footer from '../components/Footer/Footer.jsx';
 
 export default class ProfilePage extends Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-      events: {},
-    };
-
-    this.addEvent = this.addEvent.bind(this);
-  }
-
-  addEvent(title, desc, url) {
-// console.log('title....', title);
-    fetch('./events', {
-      method: 'POST',
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      },
-      body: JSON.stringify({title, desc, url})
-// console.log('body....', body);
-    })
-      .then((r) => {
-        console.log('response (r) is: ', r);
-        r.json();
-      })
-      .then((newEvent) => {
-// console.log('newEvent....', newEvent);
-        // clone existing state
-        const newState = {...this.state.events};
-        newState[newEvent.id] = newEvent;
-        this.setState({events: newState});
-        next();
-      })
-      .catch((error) => {
-        throw error;
-      });
-  }
-
   render() {
-    return (
-      <div className="ProfilePage">
-
-      <Nav/>
-        <div id="a">
-          <a href="#">Sign Out</a>
+    return(
+      <div>
+        <Nav />
+        <img src="#" alt="Profile_img"/>
+        <div className="userInfo">user info</div>
+        <div className="users_events"> user events
         </div>
 
-       <EventList
-          events={this.state.events}
-          getAllEvents={this.getAllEvents.bind(this)}
-        />
+        <div className="interests"> interests
+        </div>
+        <div className="friendList"> friends list
+        </div>
 
-        <h1>Add Event Form</h1>
-        <EventForm addEvent={this.addEvent.bind(this)}
-        />
+
         <Footer />
       </div>
-    );
+      )
   }
 }
-// add={this.addButton.bind(this)}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import ProfilePage      from './ProfilePage/ProfilePage.jsx';
+import DashboardPage      from './DashboardPage/DashboardPage.jsx';
 import LoginPage from './LoginPage/LoginPage.jsx';
 import LandingPage from './LandingPage/LandingPage.jsx';
+import ProfilePage from './ProfilePage/ProfilePage.jsx';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={LandingPage}></Route>
-    <Route path='/Signup_Login' component={LoginPage}></Route>
-    <Route path='/ProfilePage' component={ProfilePage}></Route>
+    <Route path='/signup_login' component={LoginPage}></Route>
+    <Route path='/dashboard' component={DashboardPage}></Route>
+    <Route path='/profile' component={ProfilePage}></Route>
 </Router>, document.querySelector('#root-container'));
