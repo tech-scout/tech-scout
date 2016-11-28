2f7f39a9e2c21278816f177e330705edb995d531
Imani Fecu <imani.fecu72@ymail.com>
2016-11-27 19:45:47 -0500

    changes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,7 +5,7 @@ export default class Search extends Component {
   render(){
     return(
       <div>
-      <h3>Search for a tech event near you</h3>
+      <h4>Search for a tech event near you</h4>
       <input id="search" type="text" placeholder="Search"/>
       <input id="search_button" value="Search" type="submit"/>
       </div>
