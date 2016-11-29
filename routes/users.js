5dec9b7cf8dcba81b2775b6bcfd195014e2b5422
Paris Leach <parisleach@gmail.com>
2016-11-28 19:44:57 -0500

    auth nearing completion

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,12 @@ const sendJSONresp = (req, res) => res.json({token: res.token, body: res.rows});
 // events
 // this is the most general route, so it goes last
 users.route('/')
-  .post(db.createUser, sendJSONresp)
+  .post(db.createUser, auth.createToken, (req, res) => {
+    // console.log(res.token);
+    res.json({token: res.token});
+  })
   .get(db.getUser, auth.checkPassword, sendJSONresp);
+  // .get((req, res) => res.send(200));
 
 // export this so it is available to server.js
 module.exports = users;
--- src/components/Form_Sign/Form_Sign.jsx
+++ src/components/Form_Sign/Form_Sign.jsx
@@ -2,16 +2,52 @@ import React, { Component } from 'react';
 import './Form_Sign.css'
 
 export default class Form_Sign extends Component {
+
+  constructor(props) {
+
+    super();
+    this.state = {
+      username: '',
+      title: '',
+      img_url: '',
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(r => r.json())
+    .then((data) => {
+      localStorage.setItem('techScoutToken', data);
+    });
+  }
+
   render() {
     return(
       <div className="signup_form">
-        <form action="/app/profile" method="POST">
-          <input type="text" name="username" placeholder="username"/>
-          <input type="text" name="title" placeholder="professional title"/>
-          <input type="text" name="img_url" placeholder="url for profile picture"/>
-          <input type="text" name="email" placeholder="Email"/>
-          <input type="text" name="password" placeholder="password"/>
-          <input value="Sign Up" id="submit" type="submit"/>
+        <form action="../api/users" method="POST">
+          <input type="text" value={this.state.username} onChange={this.handleChange} name="username" placeholder="username"/>
+          <input type="text" value={this.state.title} onChange={this.handleChange} name="title" placeholder="professional title"/>
+          <input type="text" value={this.state.img_url} onChange={this.handleChange} name="img_url" placeholder="url for profile picture"/>
+          <input type="text" value={this.state.email} onChange={this.handleChange} name="email" placeholder="Email"/>
+          <input type="text" value={this.state.password} onChange={this.handleChange} name="password" placeholder="password"/>
+          <input value="Sign Up" onClick={this.handleSubmit} id="submit" type="submit"/>
         </form>
       </div>
       )
